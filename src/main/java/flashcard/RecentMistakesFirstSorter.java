65ff0dd9dc6cc8ac36d2399e1942dd1725f97345
Oogii852 <otgonsukh0311@gmail.com>
2026-04-22 17:24:14 +0800

    bie daaalt

--- src/main/java/flashcard/RecentMistakesFirstSorter.java
+++ src/main/java/flashcard/RecentMistakesFirstSorter.java
@@ -1,23 +1,31 @@
 package flashcard;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class RecentMistakesFirstSorter implements CardOrganizer {
+
+    public RecentMistakesFirstSorter() {}
+
     @Override
     public List<Card> organize(List<Card> cards) {
-        List<Card> wrong = new ArrayList<>();
-        List<Card> correct = new ArrayList<>();
+        List<Card> wrongCards = new ArrayList<>();
+        List<Card> otherCards = new ArrayList<>();
 
         for (Card card : cards) {
             if (card.wasWrongLastRound()) {
-                wrong.add(card);
+                wrongCards.add(card);
             } else {
-                correct.add(card);
+                otherCards.add(card);
             }
         }
 
-        wrong.addAll(correct);
-        return wrong;
+        Collections.reverse(wrongCards);
+
+        List<Card> result = new ArrayList<>();
+        result.addAll(wrongCards);
+        result.addAll(otherCards);
+        return result;
     }
 }
